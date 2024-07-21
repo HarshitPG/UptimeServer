67627a1ba5b434d83efed0b180e28d3fba0b1876
Harshit P G <harshitpg2004@gmail.com>
2024-07-21 11:00:30 +0530

    deployment fix

--- api/main.go
+++ api/main.go
@@ -1,4 +1,4 @@
-package handler
+package main
 
 import (
 	"fmt"
@@ -9,44 +9,52 @@ import (
 )
 
 var (
-    lastPingStatus string
+	lastPingStatus string
 )
 
-func init() {
-    url := "https://zmt3q4-8080.csb.app/health" 
-
-    pingURL := func() {
-        resp, err := http.Get(url)
-        if err != nil {
-            lastPingStatus = fmt.Sprintf("Error pinging URL: %s", err)
-            fmt.Printf("Error pinging URL: %s\n", err)
-            return
-        }
-        defer resp.Body.Close()
-        lastPingStatus = fmt.Sprintf("Pinged %s - Status Code: %d", url, resp.StatusCode)
-        fmt.Printf("Pinged %s - Status Code: %d\n", url, resp.StatusCode)
-    }
-
-    pingURL()
-
-    ticker := time.NewTicker(3 * time.Minute)
-    defer ticker.Stop()
-
-    go func() {
-        for {
-            select {
-            case <-ticker.C:
-                pingURL()
-            }
-        }
-    }()
+func pingURL(url string) {
+	resp, err := http.Get(url)
+	if err != nil {
+		lastPingStatus = fmt.Sprintf("Error pinging URL: %s", err)
+		fmt.Printf("Error pinging URL: %s\n", err)
+		return
+	}
+	defer resp.Body.Close()
+	lastPingStatus = fmt.Sprintf("Pinged %s - Status Code: %d", url, resp.StatusCode)
+	fmt.Printf("Pinged %s - Status Code: %d\n", url, resp.StatusCode)
 }
 
+func startPing(url string) {
+	pingURL(url)
+	ticker := time.NewTicker(3 * time.Minute)
+	defer ticker.Stop()
+
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				pingURL(url)
+			}
+		}
+	}()
+}
+
+func main() {
+	url := "https://zmt3q4-8080.csb.app/health"
+	startPing(url)
+
+	e := echo.New()
+	e.GET("/status", func(c echo.Context) error {
+		return c.String(http.StatusOK, fmt.Sprintf("Server is running. Last ping status: %s\n", lastPingStatus))
+	})
+
+	e.Logger.Fatal(e.Start(":3000"))
+}
 
 func Handler(w http.ResponseWriter, r *http.Request) {
-    e := echo.New()
-    e.GET("/status", func(c echo.Context) error {
-        return c.String(http.StatusOK, fmt.Sprintf("Server is running. Last ping status: %s\n", lastPingStatus))
-    })
-    e.ServeHTTP(w, r)
+	e := echo.New()
+	e.GET("/status", func(c echo.Context) error {
+		return c.String(http.StatusOK, fmt.Sprintf("Server is running. Last ping status: %s\n", lastPingStatus))
+	})
+	e.ServeHTTP(w, r)
 }
