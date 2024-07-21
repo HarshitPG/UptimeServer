4eb6b74b81fbba4f050ba9cb4b78c216fb988443
Harshit P G <harshitpg2004@gmail.com>
2024-07-21 10:06:49 +0530

    added vercel config

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module uptimeServer
 
 go 1.22.2
+
+require github.com/tbxark/g4vercel v0.0.4 // indirect
--- main.go
+++ main.go
@@ -26,10 +26,27 @@ func main() {
 	ticker := time.NewTicker(3 * time.Minute)
 	defer ticker.Stop()
 
-	for {
-		select {
-		case <-ticker.C:
-			pingURL()
-		}
-	}
+    go func() {
+		for {
+            select {
+            case <-ticker.C:
+                pingURL()
+            }
+        }
+	}() 
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Go server is running!")
+	})
+
+	
+	http.HandleFunc("/status", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Server is up and running! Pinging URL: %s", url)
+	})
+
+
+	fmt.Println("Starting server on :8080")
+    if err := http.ListenAndServe(":8080", nil); err != nil {
+        fmt.Printf("Error starting server: %s\n", err)
+    }
 }
